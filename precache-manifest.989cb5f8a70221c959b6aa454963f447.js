9ec4c5435ce6b141ec25b4a5742943152463b914
Saljo <saljo@dexlock.com>
2020-04-12 12:48:04 +0530

    Updates

---
+++ precache-manifest.989cb5f8a70221c959b6aa454963f447.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c81253b937ff5ea34e4d387f301ca1",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "49faf3f16cfb8c061706",
+    "url": "/todo-app/static/css/main.d25752ad.chunk.css"
+  },
+  {
+    "revision": "9136e756eefb0d6d602b",
+    "url": "/todo-app/static/js/2.387ddfea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.387ddfea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49faf3f16cfb8c061706",
+    "url": "/todo-app/static/js/main.0008722e.chunk.js"
+  },
+  {
+    "revision": "aed0306b7a4838737999",
+    "url": "/todo-app/static/js/runtime-main.3c9e9223.js"
+  },
+  {
+    "revision": "e3c2d53dcbd6a23cfe889caedaf52cf5",
+    "url": "/todo-app/static/media/list.e3c2d53d.svg"
+  },
+  {
+    "revision": "b7306f809844309fc31da37783e031f7",
+    "url": "/todo-app/static/media/plus_2.b7306f80.svg"
+  }
+]);
